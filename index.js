bb34b1c1f04bf3540b7afa35ad1ab3fad3b847bc
Maxmillian-Muiruri <maxmillianmuiruri@gmail.com>
2023-04-20 10:46:16 +0300

    using getElementById()

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
-// getElementById()
 const title = document.getElementById("main-heading");
 
 console.log(title);
+
+// GetElementByClassName()
+const listItem = document.getElementsByClassName("list-items");
+console.log(listItem);
+
+// getElementByTagName()
+const listItems = document.getElementsByTagName("li");
+console.log(listItems);
+
+// querySelector()
+// const container = document.querySelector("div");
+
+// console.log(container);
+
+// querySelectorAll();
+
+const container = document.querySelectorAll("div");
+
+console.log(container);
